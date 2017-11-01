cec3e5e893004ebdfea1e7076aed002142e8bc4c
soywiz <soywiz@gmail.com>
2017-11-01 14:52:14 +0100

    Test refactor

--- kpspemu/common/src/test/kotlin/KpspTests.kt
+++ kpspemu/common/src/test/kotlin/KpspTests.kt
@@ -2,5 +2,6 @@ import com.soywiz.korio.vfs.localCurrentDirVfs
 
 object KpspTests {
 	val root = localCurrentDirVfs["../.."].jail()
+	val pspautotests = root["pspautotests"]
 	val rootTestResources = root["kpspemu/common/src/test/resources"]
 }
--- kpspemu/common/src/test/kotlin/com/soywiz/kpspemu/IntegrationTests.kt
+++ kpspemu/common/src/test/kotlin/com/soywiz/kpspemu/IntegrationTests.kt
@@ -1,10 +1,10 @@
 package com.soywiz.kpspemu
 
+import KpspTests
 import MyAssert
 import com.soywiz.korio.async.syncTest
 import com.soywiz.korio.lang.Console
 import com.soywiz.korio.stream.SyncStream
-import com.soywiz.korio.vfs.localCurrentDirVfs
 import com.soywiz.kpspemu.format.elf.loadElfAndSetRegisters
 import com.soywiz.kpspemu.hle.registerNativeModules
 import com.soywiz.kpspemu.util.PspLogLevel
@@ -15,6 +15,34 @@ import org.junit.Test
 
 class IntegrationTests {
 	val TRACE = false
+
+	@Test fun testCpuAlu() = testFile("cpu/cpu_alu/cpu_alu")
+	@Test fun testCpuBranch() = testFile("cpu/cpu_alu/cpu_branch")
+	@Test fun testCpuBranch2() = testFile("cpu/cpu_alu/cpu_branch2")
+
+	@Test fun testIcache() = testFile("cpu/icache/icache")
+
+	@Test fun testLsu() = testFile("cpu/lsu/lsu")
+
+	@Test fun testFpu() = testFile("cpu/fpu/fpu", ignores = listOf(
+		"mul.s 0.296558 * 62.000000, CAST_1 = 18.38657^",
+		"mul.s 0.296558 * 62.000000, FLOOR_3 = 18.38657^"
+	))
+
+	@Test fun testFcr() = testFile("cpu/fpu/fcr", ignores = listOf(
+		"Underflow:\n  fcr0: 00003351, fcr25: 00000000, fcr26: 00000000, fcr27: 00000000, fcr28: 00000000, fcr31: ^^^^^^^^",
+		"Inexact:\n  fcr0: 00003351, fcr25: 00000000, fcr26: 00000000, fcr27: 00000000, fcr28: 00000000, fcr31: ^^^^^^^^"
+	))
+
+	@Test fun testRtc() = testFile("rtc/rtc")
+
+	//@Test fun testThreadsK0() = testFile("threads/k0/k0")
+
+	//@Test fun testVfpuColors() = testFile("cpu/vfpu/colors")
+
+	//@Test fun testFpuFpu() = testFile("cpu/fpu/fpu")
+	//@Test fun testCpuBranch() = testFile("cpu/cpu_alu/cpu_branch")
+	//@Test fun testCpuBranch2() = testFile("cpu/cpu_alu/cpu_branch2")
 
 	suspend fun testFile(elf: SyncStream, expected: String, ignores: List<String>, processor: (String) -> String = { it }) {
 		val emulator = Emulator()
@@ -56,42 +84,15 @@ class IntegrationTests {
 			return out
 		}
 		MyAssert.assertEquals(expected.normalize(), processor(emulator.output.toString().normalize()))
+		//assertEquals(expected.normalize(), processor(emulator.output.toString().normalize()))
 	}
 
 	fun testFile(name: String, ignores: List<String> = listOf(), processor: (String) -> String = { it }) = syncTest {
 		testFile(
-			localCurrentDirVfs["../../pspautotests/$name.prx"].readAsSyncStream(),
-			localCurrentDirVfs["../../pspautotests/$name.expected"].readString(),
+			KpspTests.pspautotests["$name.prx"].readAsSyncStream(),
+			KpspTests.pspautotests["$name.expected"].readString(),
 			ignores,
 			processor
 		)
 	}
-
-	@Test fun testCpuAlu() = testFile("cpu/cpu_alu/cpu_alu")
-	@Test fun testCpuBranch() = testFile("cpu/cpu_alu/cpu_branch")
-	@Test fun testCpuBranch2() = testFile("cpu/cpu_alu/cpu_branch2")
-
-	@Test fun testIcache() = testFile("cpu/icache/icache")
-
-	@Test fun testLsu() = testFile("cpu/lsu/lsu")
-
-	@Test fun testFpu() = testFile("cpu/fpu/fpu", ignores = listOf(
-		"mul.s 0.296558 * 62.000000, CAST_1 = 18.38657^",
-		"mul.s 0.296558 * 62.000000, FLOOR_3 = 18.38657^"
-	))
-
-	@Test fun testFcr() = testFile("cpu/fpu/fcr", ignores = listOf(
-		"Underflow:\n  fcr0: 00003351, fcr25: 00000000, fcr26: 00000000, fcr27: 00000000, fcr28: 00000000, fcr31: ^^^^^^^^",
-		"Inexact:\n  fcr0: 00003351, fcr25: 00000000, fcr26: 00000000, fcr27: 00000000, fcr28: 00000000, fcr31: ^^^^^^^^"
-	))
-
-	@Test fun testRtc() = testFile("rtc/rtc")
-
-	//@Test fun testThreadsK0() = testFile("threads/k0/k0")
-
-	//@Test fun testVfpuColors() = testFile("cpu/vfpu/colors")
-
-	//@Test fun testFpuFpu() = testFile("cpu/fpu/fpu")
-	//@Test fun testCpuBranch() = testFile("cpu/cpu_alu/cpu_branch")
-	//@Test fun testCpuBranch2() = testFile("cpu/cpu_alu/cpu_branch2")
 }
