6fde133f08bdd12cb40f6ca483441cc955498c54
soywiz <soywiz@gmail.com>
2017-11-15 02:44:15 +0100

    Enable fast registers for interpreter since dynarek already understand getters and setters

--- kpspemu/common/src/main/kotlin/com/soywiz/kpspemu/cpu/CpuState.kt
+++ kpspemu/common/src/main/kotlin/com/soywiz/kpspemu/cpu/CpuState.kt
@@ -63,98 +63,98 @@ class CpuState(val globalCpuState: GlobalCpuState, val mem: Memory, val syscalls
 		}
 	}
 
-	// @TODO: Fast version for dynarek (required until implemented property getter/setters)
+	// @TODO: Fast version for dynarek
 
-	@JvmField @JsName("r0") var r0: Int = 0
-	@JvmField @JsName("r1") var r1: Int = 0
-	@JvmField @JsName("r2") var r2: Int = 0
-	@JvmField @JsName("r3") var r3: Int = 0
-	@JvmField @JsName("r4") var r4: Int = 0
-	@JvmField @JsName("r5") var r5: Int = 0
-	@JvmField @JsName("r6") var r6: Int = 0
-	@JvmField @JsName("r7") var r7: Int = 0
-	@JvmField @JsName("r8") var r8: Int = 0
-	@JvmField @JsName("r9") var r9: Int = 0
-	@JvmField @JsName("r10") var r10: Int = 0
-	@JvmField @JsName("r11") var r11: Int = 0
-	@JvmField @JsName("r12") var r12: Int = 0
-	@JvmField @JsName("r13") var r13: Int = 0
-	@JvmField @JsName("r14") var r14: Int = 0
-	@JvmField @JsName("r15") var r15: Int = 0
-	@JvmField @JsName("r16") var r16: Int = 0
-	@JvmField @JsName("r17") var r17: Int = 0
-	@JvmField @JsName("r18") var r18: Int = 0
-	@JvmField @JsName("r19") var r19: Int = 0
-	@JvmField @JsName("r20") var r20: Int = 0
-	@JvmField @JsName("r21") var r21: Int = 0
-	@JvmField @JsName("r22") var r22: Int = 0
-	@JvmField @JsName("r23") var r23: Int = 0
-	@JvmField @JsName("r24") var r24: Int = 0
-	@JvmField @JsName("r25") var r25: Int = 0
-	@JvmField @JsName("r26") var r26: Int = 0
-	@JvmField @JsName("r27") var r27: Int = 0
-	@JvmField @JsName("r28") var r28: Int = 0
-	@JvmField @JsName("r29") var r29: Int = 0
-	@JvmField @JsName("r30") var r30: Int = 0
-	@JvmField @JsName("r31") var r31: Int = 0
-
-	fun getGpr(index: Int): Int {
-		return when (index) {
-			0 -> 0;1 -> r1;2 -> r2;3 -> r3;4 -> r4;5 -> r5;6 -> r6;7 -> r7;
-			8 -> r8;9 -> r9;10 -> r10;11 -> r11;12 -> r12;13 -> r13;14 -> r14;15 -> r15;
-			16 -> r16;17 -> r17;18 -> r18;19 -> r19;20 -> r20;21 -> r21;22 -> r22;23 -> r23;
-			24 -> r24;25 -> r25;26 -> r26;27 -> r27;28 -> r28;29 -> r29;30 -> r30;31 -> r31;
-			else -> invalidOp
-		}
-	}
-
-	fun setGpr(index: Int, v: Int): Unit {
-		when (index) {
-			0 -> Unit;1 -> r1 = v;2 -> r2 = v;3 -> r3 = v;4 -> r4 = v;5 -> r5 = v;6 -> r6 = v;7 -> r7 = v;
-			8 -> r8 = v;9 -> r9 = v;10 -> r10 = v;11 -> r11 = v;12 -> r12 = v;13 -> r13 = v;14 -> r14 = v;15 -> r15 = v;
-			16 -> r16 = v;17 -> r17 = v;18 -> r18 = v;19 -> r19 = v;20 -> r20 = v;21 -> r21 = v;22 -> r22 = v;23 -> r23 = v;
-			24 -> r24 = v;25 -> r25 = v;26 -> r26 = v;27 -> r27 = v;28 -> r28 = v;29 -> r29 = v;30 -> r30 = v;31 -> r31 = v;
-			else -> invalidOp
-		}
-	}
+	//@JvmField @JsName("r0") var r0: Int = 0
+	//@JvmField @JsName("r1") var r1: Int = 0
+	//@JvmField @JsName("r2") var r2: Int = 0
+	//@JvmField @JsName("r3") var r3: Int = 0
+	//@JvmField @JsName("r4") var r4: Int = 0
+	//@JvmField @JsName("r5") var r5: Int = 0
+	//@JvmField @JsName("r6") var r6: Int = 0
+	//@JvmField @JsName("r7") var r7: Int = 0
+	//@JvmField @JsName("r8") var r8: Int = 0
+	//@JvmField @JsName("r9") var r9: Int = 0
+	//@JvmField @JsName("r10") var r10: Int = 0
+	//@JvmField @JsName("r11") var r11: Int = 0
+	//@JvmField @JsName("r12") var r12: Int = 0
+	//@JvmField @JsName("r13") var r13: Int = 0
+	//@JvmField @JsName("r14") var r14: Int = 0
+	//@JvmField @JsName("r15") var r15: Int = 0
+	//@JvmField @JsName("r16") var r16: Int = 0
+	//@JvmField @JsName("r17") var r17: Int = 0
+	//@JvmField @JsName("r18") var r18: Int = 0
+	//@JvmField @JsName("r19") var r19: Int = 0
+	//@JvmField @JsName("r20") var r20: Int = 0
+	//@JvmField @JsName("r21") var r21: Int = 0
+	//@JvmField @JsName("r22") var r22: Int = 0
+	//@JvmField @JsName("r23") var r23: Int = 0
+	//@JvmField @JsName("r24") var r24: Int = 0
+	//@JvmField @JsName("r25") var r25: Int = 0
+	//@JvmField @JsName("r26") var r26: Int = 0
+	//@JvmField @JsName("r27") var r27: Int = 0
+	//@JvmField @JsName("r28") var r28: Int = 0
+	//@JvmField @JsName("r29") var r29: Int = 0
+	//@JvmField @JsName("r30") var r30: Int = 0
+	//@JvmField @JsName("r31") var r31: Int = 0
+//
+	//fun getGpr(index: Int): Int {
+	//	return when (index) {
+	//		0 -> 0;1 -> r1;2 -> r2;3 -> r3;4 -> r4;5 -> r5;6 -> r6;7 -> r7;
+	//		8 -> r8;9 -> r9;10 -> r10;11 -> r11;12 -> r12;13 -> r13;14 -> r14;15 -> r15;
+	//		16 -> r16;17 -> r17;18 -> r18;19 -> r19;20 -> r20;21 -> r21;22 -> r22;23 -> r23;
+	//		24 -> r24;25 -> r25;26 -> r26;27 -> r27;28 -> r28;29 -> r29;30 -> r30;31 -> r31;
+	//		else -> invalidOp
+	//	}
+	//}
+//
+	//fun setGpr(index: Int, v: Int): Unit {
+	//	when (index) {
+	//		0 -> Unit;1 -> r1 = v;2 -> r2 = v;3 -> r3 = v;4 -> r4 = v;5 -> r5 = v;6 -> r6 = v;7 -> r7 = v;
+	//		8 -> r8 = v;9 -> r9 = v;10 -> r10 = v;11 -> r11 = v;12 -> r12 = v;13 -> r13 = v;14 -> r14 = v;15 -> r15 = v;
+	//		16 -> r16 = v;17 -> r17 = v;18 -> r18 = v;19 -> r19 = v;20 -> r20 = v;21 -> r21 = v;22 -> r22 = v;23 -> r23 = v;
+	//		24 -> r24 = v;25 -> r25 = v;26 -> r26 = v;27 -> r27 = v;28 -> r28 = v;29 -> r29 = v;30 -> r30 = v;31 -> r31 = v;
+	//		else -> invalidOp
+	//	}
+	//}
 
 	// @TODO: Fast version for interpreted
 
-	//var _R = IntArray(32)
-	//var r0: Int; set(value) = Unit; get() = 0
-	//var r1: Int; set(value) = run { _R[1] = value }; get() = _R[1]
-	//var r2: Int; set(value) = run { _R[2] = value }; get() = _R[2]
-	//var r3: Int; set(value) = run { _R[3] = value }; get() = _R[3]
-	//var r4: Int; set(value) = run { _R[4] = value }; get() = _R[4]
-	//var r5: Int; set(value) = run { _R[5] = value }; get() = _R[5]
-	//var r6: Int; set(value) = run { _R[6] = value }; get() = _R[6]
-	//var r7: Int; set(value) = run { _R[7] = value }; get() = _R[7]
-	//var r8: Int; set(value) = run { _R[8] = value }; get() = _R[8]
-	//var r9: Int; set(value) = run { _R[9] = value }; get() = _R[9]
-	//var r10: Int; set(value) = run { _R[10] = value }; get() = _R[10]
-	//var r11: Int; set(value) = run { _R[11] = value }; get() = _R[11]
-	//var r12: Int; set(value) = run { _R[12] = value }; get() = _R[12]
-	//var r13: Int; set(value) = run { _R[13] = value }; get() = _R[13]
-	//var r14: Int; set(value) = run { _R[14] = value }; get() = _R[14]
-	//var r15: Int; set(value) = run { _R[15] = value }; get() = _R[15]
-	//var r16: Int; set(value) = run { _R[16] = value }; get() = _R[16]
-	//var r17: Int; set(value) = run { _R[17] = value }; get() = _R[17]
-	//var r18: Int; set(value) = run { _R[18] = value }; get() = _R[18]
-	//var r19: Int; set(value) = run { _R[19] = value }; get() = _R[19]
-	//var r20: Int; set(value) = run { _R[20] = value }; get() = _R[20]
-	//var r21: Int; set(value) = run { _R[21] = value }; get() = _R[21]
-	//var r22: Int; set(value) = run { _R[22] = value }; get() = _R[22]
-	//var r23: Int; set(value) = run { _R[23] = value }; get() = _R[23]
-	//var r24: Int; set(value) = run { _R[24] = value }; get() = _R[24]
-	//var r25: Int; set(value) = run { _R[25] = value }; get() = _R[25]
-	//var r26: Int; set(value) = run { _R[26] = value }; get() = _R[26]
-	//var r27: Int; set(value) = run { _R[27] = value }; get() = _R[27]
-	//var r28: Int; set(value) = run { _R[28] = value }; get() = _R[28]
-	//var r29: Int; set(value) = run { _R[29] = value }; get() = _R[29]
-	//var r30: Int; set(value) = run { _R[30] = value }; get() = _R[30]
-	//var r31: Int; set(value) = run { _R[31] = value }; get() = _R[31]
-	//fun getGpr(index: Int): Int = _R[index]
-	//fun setGpr(index: Int, v: Int): Unit = run { if (index != 0) _R[index] = v }
+	var _R = IntArray(32)
+	var r0: Int; set(value) = Unit; get() = 0
+	var r1: Int; set(value) = run { _R[1] = value }; get() = _R[1]
+	var r2: Int; set(value) = run { _R[2] = value }; get() = _R[2]
+	var r3: Int; set(value) = run { _R[3] = value }; get() = _R[3]
+	var r4: Int; set(value) = run { _R[4] = value }; get() = _R[4]
+	var r5: Int; set(value) = run { _R[5] = value }; get() = _R[5]
+	var r6: Int; set(value) = run { _R[6] = value }; get() = _R[6]
+	var r7: Int; set(value) = run { _R[7] = value }; get() = _R[7]
+	var r8: Int; set(value) = run { _R[8] = value }; get() = _R[8]
+	var r9: Int; set(value) = run { _R[9] = value }; get() = _R[9]
+	var r10: Int; set(value) = run { _R[10] = value }; get() = _R[10]
+	var r11: Int; set(value) = run { _R[11] = value }; get() = _R[11]
+	var r12: Int; set(value) = run { _R[12] = value }; get() = _R[12]
+	var r13: Int; set(value) = run { _R[13] = value }; get() = _R[13]
+	var r14: Int; set(value) = run { _R[14] = value }; get() = _R[14]
+	var r15: Int; set(value) = run { _R[15] = value }; get() = _R[15]
+	var r16: Int; set(value) = run { _R[16] = value }; get() = _R[16]
+	var r17: Int; set(value) = run { _R[17] = value }; get() = _R[17]
+	var r18: Int; set(value) = run { _R[18] = value }; get() = _R[18]
+	var r19: Int; set(value) = run { _R[19] = value }; get() = _R[19]
+	var r20: Int; set(value) = run { _R[20] = value }; get() = _R[20]
+	var r21: Int; set(value) = run { _R[21] = value }; get() = _R[21]
+	var r22: Int; set(value) = run { _R[22] = value }; get() = _R[22]
+	var r23: Int; set(value) = run { _R[23] = value }; get() = _R[23]
+	var r24: Int; set(value) = run { _R[24] = value }; get() = _R[24]
+	var r25: Int; set(value) = run { _R[25] = value }; get() = _R[25]
+	var r26: Int; set(value) = run { _R[26] = value }; get() = _R[26]
+	var r27: Int; set(value) = run { _R[27] = value }; get() = _R[27]
+	var r28: Int; set(value) = run { _R[28] = value }; get() = _R[28]
+	var r29: Int; set(value) = run { _R[29] = value }; get() = _R[29]
+	var r30: Int; set(value) = run { _R[30] = value }; get() = _R[30]
+	var r31: Int; set(value) = run { _R[31] = value }; get() = _R[31]
+	fun getGpr(index: Int): Int = _R[index]
+	fun setGpr(index: Int, v: Int): Unit = run { if (index != 0) _R[index] = v }
 
 
 	//val FPR = FloatArray(32) { 0f }
